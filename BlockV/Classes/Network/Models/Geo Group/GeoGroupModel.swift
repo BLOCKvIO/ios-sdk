563f7db0797eb1e51896477aad26ab6ddd459ebb
Cameron McOnie <cjmconie@gmail.com>
2018-07-04 13:53:58 +0200

    operator_whitespace

--- BlockV/Classes/Network/Models/Geo Group/GeoGroupModel.swift
+++ BlockV/Classes/Network/Models/Geo Group/GeoGroupModel.swift
@@ -79,6 +79,6 @@ extension GeoGroupModel: Codable {
 
 extension CLLocationCoordinate2D: Equatable {}
 
-public func ==(lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+public func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
     return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
 }

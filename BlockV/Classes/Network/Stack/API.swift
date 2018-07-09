7bb9361e224fc06f4499bd2489ba8aa1e28f815d
Cameron McOnie <cjmconie@gmail.com>
2018-07-09 16:24:23 +0200

    rename delete as trash to match the platform's language

--- BlockV/Classes/Network/Stack/API.swift
+++ BlockV/Classes/Network/Stack/API.swift
@@ -278,10 +278,10 @@ extension API {
             )
         }
 
-        /// Builds the endpoint to delete a vAtom specified by its id.
+        /// Builds the endpoint to trash a vAtom specified by its id.
         ///
         /// Returns an endpoint over a BaseModel over a GeneralModel.
-        static func deleteVatom(_ id: String) -> Endpoint<BaseModel<GeneralModel>> {
+        static func trashVatom(_ id: String) -> Endpoint<BaseModel<GeneralModel>> {
             return Endpoint(method: .post,
                             path: "/v1/user/vatom/trash",
                             parameters: ["this.id": id])

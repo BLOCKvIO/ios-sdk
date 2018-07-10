d938457395e4ca1a0dfda43650592c76f061b637
Cameron McOnie <cjmconie@gmail.com>
2018-07-10 12:45:23 +0200

    fix colon spacing

--- BlockV/Classes/Network/Params/TokenRegisterParams.swift
+++ BlockV/Classes/Network/Params/TokenRegisterParams.swift
@@ -62,7 +62,7 @@ extension UserInfo: DictionaryCodable {
             "last_name": lastName ?? "",
             "name_public": isNamePublic ?? true,
             "password": password ?? "",
-            "birthday" : birthday ?? "",
+            "birthday": birthday ?? "",
             "avatar_public": isAvatarPublic ?? true,
             "language": language ?? ""
         ]

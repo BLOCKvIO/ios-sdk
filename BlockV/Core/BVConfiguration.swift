bba9b784bf0383ccb6320b982f1e714655a69b34
Cameron McOnie <cjmconie@gmail.com>
2018-07-27 12:15:03 +0200

    update configuration file

--- BlockV/Core/BVConfiguration.swift
+++ BlockV/Core/BVConfiguration.swift
@@ -11,37 +11,27 @@
 
 import Foundation
 
-class BVConfiguration {
-
-    var appID: String?
-
-    /// Models the BLOCKv platform environments.
-    ///
-    /// Options:
-    /// - production.
-    /// - developement (Unstable - DO NOT USE).
-    public enum BVEnvironment {
-        /// Stable production environment.
-        case production
-        /// Unstable development environement (DO NOT USE).
-        case development
-
-        /// BLOCKv server base url
-        var apiServerURLString: String {
-            switch self {
-            case .production:  return "https://api.blockv.io"
-            case .development: return "https://apidev.blockv.net"
-            }
+/// Models the BLOCKv platform environments.
+public enum BVEnvironment: String {
+    /// Stable production environment.
+    case production = "prod_env"
+    /// Unstable development environement (DO NOT USE).
+    case development = "dev_env"
+    
+    /// BLOCKv server base url
+    var apiServerURLString: String {
+        switch self {
+        case .production:  return "https://api.blockv.io"
+        case .development: return "https://apidev.blockv.net"
         }
-
-        /// BLOCKv Web socket server base url
-        var webSocketURLString: String {
-            switch self {
-            case .production:  return "wss//newws.blockv.io/ws"
-            case .development: return "wss://ws.blockv.net/ws"
-            }
-        }
-
     }
-
+    
+    /// BLOCKv Web socket server base url
+    var webSocketURLString: String {
+        switch self {
+        case .production:  return "wss//newws.blockv.io/ws"
+        case .development: return "wss://ws.blockv.net/ws"
+        }
+    }
+    
 }

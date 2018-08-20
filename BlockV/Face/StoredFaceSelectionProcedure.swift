44124646e4d2f0d535ea48ee3f8537ec335ecb34
Cameron McOnie <cjmconie@gmail.com>
2018-08-20 10:41:03 +0200

    refine stored face selection procedures

---
+++ BlockV/Face/StoredFaceSelectionProcedure.swift
@@ -0,0 +1,223 @@
+//
+//  BlockV AG. Copyright (c) 2018, all rights reserved.
+//
+//  Licensed under the BlockV SDK License (the "License"); you may not use this file or
+//  the BlockV SDK except in compliance with the License accompanying it. Unless
+//  required by applicable law or agreed to in writing, the BlockV SDK distributed under
+//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+//  ANY KIND, either express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+//
+
+import Foundation
+
+/*
+ Goals:
+ 1. Vatom View will ask for the best face (default routine for each view context).
+ 2. Viewer's must be able to use pre-defined routines.
+ 3. Viewer's must be able supply a custom face selection procedure.
+ 
+ Concept: Face Selection Procedure
+ 
+ A face selection procedure is an algorithm used to select a face model from the (potentially) many faces
+ associated with the vatom's template.
+ 
+ Since vatoms rarely define the exact face they wish to show (because the faces that get registered against the vatom's
+ template are out of the developers control).
+ 
+ A face selection procedure allows for 2 things:
+ 1. The best face can be chosen from the attributes and contraints of the available faces.
+ 2. A fallback face can be provided (in the event no face meets the criteria).
+ 
+ 
+ Face selection procedures ONLY validate:
+ 1. The native face code is installed.
+ 2. The platform is supported.
+ 3. The constrians, e.g. view mode are satisfied.
+ 
+ > If there are multiple, select the first.
+ 
+ Face selection routines do NOT validate:
+ 1. Vatom private properties
+ 2. Vatom resources
+ 
+ > Rather, such errors are left to the face code to validate and display an error.
+ */
+
+// MARK: - Typealias
+
+/// Closure used during face model selection.
+///
+/// A Face Selection Procedure (FSP) is an algorithm used to select the 'best' face model from the (potentially) many
+/// faces associated with the vatom's template. An FSP gives Viewers the control over the selection procedure.
+///
+/// - Parameters:
+///   - vatom: The vAtom whose faces are to be selected.
+///   - actions: The action models associated with the vAtom's template.
+///   - faces: The face models assocated with the vAtom's template.
+typealias FaceSelectionProcedure = (_ vatom: VatomModel, _ actions: [ActionModel], _ faces: [FaceModel])
+    -> FaceModel?
+
+// MARK: -
+
+/// Models the face selection procedures (FSP)s. This is a set of pre-built face selection procedures offered by the SDK
+/// to meet common use cases.
+///
+/// It important to think of the cases simply as unique identifiers of stored face selection procedures.
+/// Cases loosely map to the server's 'view_mode' simply because the 'view_mode' is generally the predominant selection
+/// criteria.
+public enum StoredProcedure: String {
+
+    case icon
+    case activated
+    case fullscreen
+    case card
+    case background
+
+    /// A fallback allows one procedure to fallback on antoher in the event the first procedure fails to select a
+    /// face model.
+    var fallback: StoredProcedure? {
+        switch self {
+        case .icon:         return nil
+        case .activated:    return .icon
+        case .fullscreen:   return .icon
+        case .card:         return .fullscreen
+        case .background:   return nil
+        }
+    }
+
+    /// Returns the selection procedure for this case.
+    var selectionProcedure: FaceSelectionProcedure {
+        switch self {
+        case .icon:         return StoredProcedureBuilder.iconProcedure
+        case .activated:    return StoredProcedureBuilder.activatedProcedure
+        case .fullscreen:   return StoredProcedureBuilder.fullscreenProcedure
+        case .card:         return StoredProcedureBuilder.cardProcedure
+        case .background:   return StoredProcedureBuilder.backgroundProcedure
+        }
+    }
+
+    // MARK: - Face selection
+
+    /// Selects the 'best' face using this procedure's stored Face Selection Procedure (FSP).
+    ///
+    /// - Parameters:
+    ///   - vatom: The vAtom to be displayed.
+    ///   - actions: The action models associated with the vAtom's template.
+    ///   - faces: The face models assocated with the vAtom's template.
+    ///   - useFallback: Determines whether the fallback procedure should be used. If `true` the fallback is used,
+    ///   `false` otherwise.
+    /// - Returns: The selected face model, or `nil` if no face model is selected.
+    func selectBestFace(vatom: VatomModel, actions: [ActionModel], faces: [FaceModel], useFallback: Bool = true)
+        -> FaceModel? {
+        // execute this procedure, use fallback if necessary
+        return self.selectionProcedure(vatom, actions, faces) ??
+            self.fallback?.selectionProcedure(vatom, actions, faces)
+    }
+
+    // MARK: Constraints
+
+    /// Constraints associated with this embedded procedure.
+    ///
+    /// Returns the constrains for this embedded procedure.
+    var constraints: SelectionConstraints {
+        return SelectionConstraints(viewMode: self.rawValue)
+    }
+
+    /// Constraints associated with this embedded procedure.
+    ///
+    /// These constraints are used as the selection criteria when choosing the best face for this procedure.
+    struct SelectionConstraints {
+        /// The view_mode of the face.
+        let viewMode: String
+        // let quality: String // e.g. of futher constraints
+    }
+
+}
+
+///
+private struct StoredProcedureBuilder {
+
+    // MARK: - Stored Face Selection Procedure (FSP)
+
+    static let iconProcedure: FaceSelectionProcedure = { (vatom, actionModels, faceModels) in
+        StoredProcedureBuilder.defaultSelectionProcedure(faceModels, StoredProcedure.icon.constraints)
+    }
+
+    static let activatedProcedure: FaceSelectionProcedure = { (vatom, actionModels, faceModels) in
+        StoredProcedureBuilder.defaultSelectionProcedure(faceModels, StoredProcedure.activated.constraints)
+    }
+
+    static let fullscreenProcedure: FaceSelectionProcedure = { (vatom, actionModels, faceModels) in
+        StoredProcedureBuilder.defaultSelectionProcedure(faceModels, StoredProcedure.fullscreen.constraints)
+    }
+
+    static let cardProcedure: FaceSelectionProcedure = { (vatom, actionModels, faceModels) in
+        StoredProcedureBuilder.defaultSelectionProcedure(faceModels, StoredProcedure.card.constraints)
+    }
+
+    static let backgroundProcedure: FaceSelectionProcedure = { (vatom, actionModels, faceModels) in
+        StoredProcedureBuilder.defaultSelectionProcedure(faceModels, StoredProcedure.background.constraints)
+    }
+
+    // MARK: - Stored Procedure
+
+    /// Stored procedures only take in the face models and a set of constraints.
+    ///
+    /// A face selection procedure takes, as input, an array of face models and a set of face constraints. As output,
+    /// it return a face model which satisfies all supplied contraints, or `nil` if no satisfactory face was found.
+    ///
+    /// - Parameters:
+    ///   - faceModels: Array of face models to be used by the selection procedure.
+    ///   - constraints: Struct holding face contraints to be used by the selection procedure.
+    typealias StoredFaceSelectionProcedure = (_ faceModels: [FaceModel],
+        _ constraints: StoredProcedure.SelectionConstraints)
+        -> FaceModel?
+
+    /// Default selection procedure.
+    ///
+    /// This closure defines a procedure that is common to most stored procedures. Therefore, it makes
+    /// sense to the logic in a central place.
+    static let defaultSelectionProcedure: StoredFaceSelectionProcedure = { (faceModels, constraints) in
+
+        var bestFace: FaceModel?
+        var bestRank = 0
+
+        for face in faceModels {
+
+            var rank = -1
+
+            // ensure the 'view mode' is supported
+            if face.properties.constraints.viewMode != constraints.viewMode {
+                rank = -1
+                continue
+            }
+
+            // rank 'ios' faces over 'generic'
+            if face.properties.constraints.platform == "ios" {
+                rank += 2
+            } else if face.properties.constraints.platform == "generic" {
+                rank += 1
+            } else {
+                rank = 1
+                continue
+            }
+
+            // rank 'native' over 'web'
+            if face.isNative {
+                rank += 1
+            }
+
+            // compare to best rank
+            if rank > bestRank {
+                bestRank = rank // update rank
+                bestFace = face // update best face
+            }
+
+        }
+
+        return bestFace
+
+    }
+
+}
--- BlockV/Face/VatomView.swift
+++ BlockV/Face/VatomView.swift
@@ -16,12 +16,11 @@ import Foundation
 /// Pack model holding a single vatom and its associated faces and actions.
 struct VatomPackModel {
     let vatom: VatomModel
-    let faces: [FaceModel]
-    let actions: [ActionModel]
+    let faces: [FaceModel] = []
+    let actions: [ActionModel] = []
     
     //TODO: The init may need to be private to prevent the viewer from init-ing this struct with arbitrary and unrelated
     // vatom, actions, or faces.
-    
 }
 
 // ------------------------------------
@@ -30,7 +29,9 @@ struct VatomPackModel {
  Questions:
  
  1. What view must be shown when the selection procedure fails to select a face?
+ > See outcomes - show error with actiavted image.
  2. Should their be a fallback, like show the native image face?
+ > No, show error.
  3. Should we allow vatom view to be instantiated directly using a face (i.e. without using a selection procedure)?
     > Maybe someone creates a viewer and only wants to add 2 faces to each of their vAtoms.
     > How are we going to enfore that people add a 'minimum reasonable faces'?
@@ -47,44 +48,48 @@ class VatomView: UIView {
     var loadingView: UIView?
     var errorView: UIView?
 
-    //TODO: This could become PackModel.
-    var vatom: VatomModel!
-    var faces: [FaceModel] = []
-    var actions: [ActionModel] = []
+    /// The vatom pack.
+    var vatomPack: VatomPackModel {
+        didSet {
+            // Run the Face View Lifecycle (FVLC).
+            runFaceViewLifecylce()
+        }
+    }
+    
+    /// The face selection procedure.
+    ///
+    /// Viewers may wish to update the procedure in reponse to certian events.
+    ///
+    /// For example, if the viewer may wish to change the procedure from 'icon' to 'activated' while the VatomView is
+    /// on screen.
+    var procedure: FaceSelectionProcedure {
+        didSet {
+            // Run the Face View Lifecycle (FVLC).
+            runFaceViewLifecylce()
+        }
+    }
+    
+    // MARK: - Web Socket
+    
+    // TODO: Respond to the Web socket, pass events down to the Face View.
+    
+    
 
     // MARK: - Initializer
-
-    // Is this needed? What is the use case for supplying a face directly?
-
-    init(vatom: VatomModel, face: FaceModel, actions: [ActionModel]) {
-
-        // create a view with a known face
-        selectedFace = face
-        super.init(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-
-    }
 
     /// Creates a vAtom view for the specifed vAtom.
     ///
     /// - Parameters:
-    ///   - vatom: The vAtom to display.
+    ///   - vatomPack: The vAtom to display and its associated faces and actions.
     ///   - faces: The array of faces associated with the vAtom's template.
     ///   - actions: The array of actions associated with the vAtom's template.
     ///   - procedure: An embedded (predefiened) face selection procedure that determines which face to display.
-    init(vatom: VatomModel,
-         faces: [FaceModel],
-         actions: [ActionModel],
-         procedure: EmbeddedProcedure) {
-
-        // select best face
-
-        //FIXME: What happens if this fails?
-        // Sould this be done on init? the procedure could be slow.
-        selectedFace = procedure.selectBestFace(from: faces)
-
+    init(vatomPack: VatomPackModel, procedure: StoredProcedure) {
+        
+        self.vatomPack = vatomPack
+        self.procedure = procedure.selectionProcedure
+        
         super.init(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-
-        print(selectedFace)
 
     }
 
@@ -95,16 +100,13 @@ class VatomView: UIView {
     ///   - faces: The array of faces associated with the vAtom's template.
     ///   - actions: The array of actions associated with the vAtom's template.
     ///   - customProcedure: A function type that allows for customization of the face selection.
-    init(vatom: VatomModel,
-         faces: [FaceModel],
-         actions: [ActionModel],
-         customProcedure: FaceSelectionProcedure) {
-
-        selectedFace = customProcedure(faces) //FIXME: What happens if this fails?
-
+    init(vatomPack: VatomPackModel,
+         customProcedure: @escaping FaceSelectionProcedure) {
+        
+        self.vatomPack = vatomPack
+        self.procedure = customProcedure
+        
         super.init(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-
-        print(selectedFace)
 
     }
 
@@ -112,13 +114,24 @@ class VatomView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setup() {
-
+    private func commonInit() {
+        
         self.loadingView = UIView() // or custom
         self.errorView = UIView() // or custom
-
+        
     }
 
     // MARK: - Methods
+    
+    func runFaceViewLifecylce() {
+        
+        if let selectedFace = procedure(self.vatomPack.vatom, self.vatomPack.actions, self.vatomPack.faces) {
+            print(selectedFace)
+            // Create and show face
+        } else {
+            // Display the error view (which shows the activated image).
+        }
+        
+    }
 
 }

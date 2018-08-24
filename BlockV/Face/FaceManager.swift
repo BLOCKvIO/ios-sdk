a98c788ebff80b1987b90ad1eb43c7a134aec07c
Cameron McOnie <cjmconie@gmail.com>
2018-08-24 14:08:52 +0200

    add face manager

---
+++ BlockV/Face/FaceManager.swift
@@ -0,0 +1,38 @@
+//
+//  BlockV AG. Copyright (c) 2018, all rights reserved.
+//
+//  Licensed under the BlockV SDK License (the "License"); you may not use this file or
+//  the BlockV SDK except in compliance with the License accompanying it. Unless
+//  required by applicable law or agreed to in writing, the BlockV SDK distributed under
+//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+//  ANY KIND, either express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+//
+
+import Foundation
+
+typealias FaceViewGenerator = (_ vatom: VatomModel, _ face: FaceModel) -> FaceView
+
+public struct FaceManager {
+
+    // MARK: - Properties
+
+    /// Dictionary of face
+    private var faceViews: [String: FaceViewGenerator] = [:]
+
+    // MARK: - Methods
+
+    /// Registers a native view generator.
+    ///
+    /// - Parameters:
+    ///   - generator: A closure that take in a vatom and a face and generates a UIView.
+    ///   - url: The display url
+    public func registerFaceView(_ faceView: FaceView) {
+
+        // register the native face somewhere
+
+//        self.faceViews[faceView.displayURL] = 
+
+    }
+
+}

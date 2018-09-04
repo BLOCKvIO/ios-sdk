75ebe9e9795e696171172af186cd4072e673048a
Cameron McOnie <cjmconie@gmail.com>
2018-09-04 22:48:52 +0200

    add activity indicator to default loader

--- BlockV/Face/Vatom View/DefaultLoadingView.swift
+++ BlockV/Face/Vatom View/DefaultLoadingView.swift
@@ -1,12 +1,56 @@
 //
-//  LoadingView.swift
-//  BLOCKv
+//  BlockV AG. Copyright (c) 2018, all rights reserved.
 //
-//  Created by Cameron McOnie on 2018/08/25.
+//  Licensed under the BlockV SDK License (the "License"); you may not use this file or
+//  the BlockV SDK except in compliance with the License accompanying it. Unless
+//  required by applicable law or agreed to in writing, the BlockV SDK distributed under
+//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+//  ANY KIND, either express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
 //
 
 import UIKit
 
-class LoadingView: UIView {
+protocol VatomLoader where Self: UIView {
+
+    func startAnimating()
+
+    func stopAnimating()
+
+}
+
+internal final class DefaultLoadingView: UIView, VatomLoader {
+
+    // MARK: - Properties
+
+    private let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        return indicator
+    }()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(activityIndicator)
+        activityIndicator.frame = self.bounds
+        activityIndicator.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.backgroundColor = UIColor.orange.withAlphaComponent(0.5)
+        self.startAnimating()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+
+    func startAnimating() {
+        activityIndicator.startAnimating()
+    }
+
+    func stopAnimating() {
+        activityIndicator.stopAnimating()
+    }
 
 }

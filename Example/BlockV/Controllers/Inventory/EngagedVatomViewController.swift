b1e3e302b9bf1bd95725b9395bdf9dacb4a4269e
Cameron McOnie <cjmconie@gmail.com>
2018-09-04 11:45:06 +0200

    add top and bottom vatom view

--- Example/BlockV/Controllers/Inventory/EngagedVatomViewController.swift
+++ Example/BlockV/Controllers/Inventory/EngagedVatomViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 import BLOCKv
 
 /*
- Challenges
- 
- - This VC is intended to show the vAtom in 'activated' view mode.
- - I would like for the activated state to be a differenct view controller. However, there must be continuity between
- the Inventory View Controller and the ActivatedViewController.
- - Options:
- 1. The ActiavtedVC has a new VatomView and the vatom pack and the fsp are passed in.
- 2. The ActivatedVC displays the same VatomView (i.e. has a reference to the VatomView), all that changes is the fsp is
- set to .actiavted. This may be a better solution interms of continuity and in terms of memory.
- 
+ Goals
+ - This VC is intended to show the how to use VatomView to visually display a vAtom.
+ - Three view are shown each presenting a VatomView.
+ - Each Vatom View has a different Face Selection Procedure (FSP).
  */
 
 class EngagedVatomViewController: UIViewController {
@@ -31,6 +25,7 @@ class EngagedVatomViewController: UIViewController {
     
     private var bottomVatomView: VatomView!
     
+    /// Backing vAtom for display.
     var vatom: VatomModel?
     
     var topProcedure: FaceSelectionProcedure?
@@ -41,41 +36,37 @@ class EngagedVatomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // VatomView using storyboard
+        // setup
+        self.title = "Vatom View"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneTapped))
+
+        // top - VatomView via storyboard
         topVatomView.update(usingVatom: vatom!, procedure: topProcedure!, roster: FaceViewRegistry.shared.roster)
         topVatomView.backgroundColor = .blue
         
-        // VatomView built in code
-        
-        // FIXME: Should we allow VatomView to be init's with no arguments?
+        // bottom - VatomView programmatically
         bottomVatomView = VatomView(vatom: vatom!, procedure: bottomProcedure!)
         bottomVatomView.backgroundColor = .red
         bottomContainerView.addSubview(bottomVatomView)
-        bottomVatomView.frame = bottomContainerView.bounds.insetBy(dx: 5, dy: 5)
+        bottomVatomView.frame = bottomContainerView.bounds.insetBy(dx: 15, dy: 15)
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneTapped))
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    // MARK: - Actions
     
     @objc private func doneTapped() {
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "seg.vatom.detail" {
+            let destination = segue.destination as! VatomDetailTableViewController
+            destination.vatom = vatomToPass
+        }
     }
-    */
 
 }

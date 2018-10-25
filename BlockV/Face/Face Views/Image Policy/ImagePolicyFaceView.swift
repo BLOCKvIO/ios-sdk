2fffcc7010a036f8cbfb85eb9467290e8b685847
Cameron McOnie <cjmconie@gmail.com>
2018-10-25 15:46:55 +0200

    add support for child policy

--- BlockV/Face/Face Views/Image Policy/ImagePolicyFaceView.swift
+++ BlockV/Face/Face Views/Image Policy/ImagePolicyFaceView.swift
@@ -17,22 +17,22 @@ import Nuke
 ///
 /// Displays a resource based on the frist matching item in the policy array.
 class ImagePolicyFaceView: FaceView {
-    
+
     class var displayURL: String { return "native://image-policy" }
-    
+
     // MARK: - Properties
-    
+
     lazy var animatedImageView: FLAnimatedImageView = {
         let imageView = FLAnimatedImageView()
         imageView.autoresizingMask = [ .flexibleWidth, .flexibleHeight ]
         imageView.clipsToBounds = true
         return imageView
     }()
-    
+
     public private(set) var isLoaded: Bool = false
-    
+
     // MARK: - Config
-    
+
     /// Face configuration (immutable).
     ///
     /// On the server, Faces and Actions are mutable (irrespective of the published state of the template). Face Views
@@ -42,161 +42,171 @@ class ImagePolicyFaceView: FaceView {
     /// Dynamically responding to face and action changes is not a function of Face Views. For this reason, the config
     /// struct immutalbe and is ONLY populated on init.
     private let config: Config
-    
+
+    /*
+     NOTE
+     The `vatomChanged()` method called by `VatomView` does not handle child vatom updates.
+     The `VatomObserver` class is used to receive these events. This is required for the Child Count policy type.
+     */
+
+    /// Class responsible for observing changes related backing vAtom.
+    private var vatomObserver: VatomObserver
+
     // MARK: - Initialization
-    
+
     required init(vatom: VatomModel, faceModel: FaceModel) {
+
+        // init face config (or legacy private section) fallback on default values
+        if let config = faceModel.properties.config {
+            self.config = Config(config) // face config
+        } else if let config = vatom.private {
+            self.config = Config(config) // private section
+        } else {
+            self.config = Config() // default values
+        }
+
+        // create an observer for the backing vatom
+        self.vatomObserver = VatomObserver(vatomID: vatom.id)
         super.init(vatom: vatom, faceModel: faceModel)
-        
+
+        self.vatomObserver.delegate = self
         // add image view
         self.addSubview(animatedImageView)
         animatedImageView.frame = self.bounds
-        
+
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) should not be called on Face Views. Please use VatomView.")
     }
-    
+
     // MARK: - Face View Lifecycle
-    
+
     /// Begin loading the face view's content.
     func load(completion: ((Error?) -> Void)?) {
-        updateResources(completion: completion)
+        updateUI()
+        completion?(nil) //FIXME: Wait for resources to download
     }
-    
+
     /// Respond to updates or replacement of the current vAtom.
     func vatomChanged(_ vatom: VatomModel) {
-        
+
         /*
          NOTE:
-         - The ImageFaceView does not have any visually dynamic attributes.
-         - The properties this face references on the vAtom are immutable after the vAtom has been emmitted.
-         - Thus, no meaningful UI update can be made.
+         - Changes to to the backing vAtom must be reflected in the face view.
          */
-        
+
         // replace current vatom
         self.vatom = vatom
-        updateResources(completion: nil)
-        
+        // FIXME: When refresh completes, the ui shoudl be updated.
+
     }
-    
+
     /// Unload the face view.
     ///
     /// Also called before reuse (when used inside a reuse pool).
-    func unload() {
-        self.animatedImageView.image = nil
-        self.animatedImageView.animatedImage = nil
-    }
-    
-    // MARK: - Resources
-    
-    /// Fetches the count of child vAtoms for the backing vAtom.
-    ///
-    /// - note:
-    /// Asynchronous network operation.
-    ///
-    /// - Parameter completetion: Completion handler to call once the the child count is known.
-    private func fetchChildCount(completion: ((Int?, Error?) -> Void)) {
-        
-        // FIXME: Hardcoded
-        completion(1, nil)
-        
-    }
-    
+    func unload() { }
+
     // MARK: - Face Code
-    
-    /*
-     Note:
-     The `vatomStateChanged()` method called by `VatomView` does not handle child vatom updates.
-     A `VatomObserver` class is used to receive these events. This is required for the Child Count policy type.
-     */
-    
-    /// Class responsible for observing changes related backing vAtom.
-    private var vatomObserver: VatomObserver
-    
+
     /// Current count of child vAtoms.
     private var currentChildCount: Int {
-        return vatomObserver.childVatomIDs.count
+        return vatomObserver.childVatoms.count
     }
-    
+
+    /// Debounced version of update UI.
+    ///
+    /// Debounced the UI update. This avoids the case where numerous parent ID state changes could cause unecessary
+    /// resource downloads. Essentially, it ensure the the vAtom is "settled" before updating the UI.
+    private lazy var debouncedUpdateUI: (() -> Void) = {
+        return debounce(delay: DispatchTimeInterval.seconds(1)) {
+            self.updateUI()
+            printBV(info: "Debounced: updateUI called")
+        }
+    }()
+
     /// Update the face view using *local* data.
     ///
     /// Loops over the array of image policies - stops if a policie's critera are satisfied and downloads the required
     /// resource and updates the image view.
     private func updateUI() {
-        
+
         // loop over polices - use first passing policy
         for policy in config.policies {
-            
+
             if let policy = policy as? Config.ChildCount {
                 // check criteria
                 if policy.countMax >= currentChildCount {
                     // update image
-                    fetchResourceNamed(policy.resourceName)
+                    resourceName = policy.resourceName
                     break
                 }
-                
-            } else if let policy = policy as? Config.FieldLookup {
-                
-                // create key path and split into head and tail
-                // only private section lookups are allowed
-                guard let component = KeyPath(policy.field).headAndTail(),
-                    component.head == "private",
-                    let vatomValue = self.vatom.properties[keyPath: component.tail] else {
-                        continue
-                }
-                
-                if  compare(vatomValue, policy.value) {
-                    // update image
-                    //print(">>:: vAtom Value: \(vatomValue) | Policy Value: \(policy.value)\n")
-                    fetchResourceNamed(policy.resourceName)
-                    break
-                }
-                
+
+//            } else if let policy = policy as? Config.FieldLookup {
+//
+//                // create key path and split into head and tail
+//                // only private section lookups are allowed
+//                guard let component = KeyPath(policy.field).headAndTail(),
+//                    component.head == "private",
+//                    let vatomValue = self.vatom.properties[keyPath: component.tail] else {
+//                        continue
+//                }
+//
+//                if  compare(vatomValue, policy.value) {
+//                    // update image
+//                    //print(">>:: vAtom Value: \(vatomValue) | Policy Value: \(policy.value)\n")
+//                    resourceName = policy.resourceName
+//                    break
+//                }
+//
             } else if policy is Config.Fallback {
                 // update image
-                fetchResourceNamed(policy.resourceName)
+                resourceName = policy.resourceName
                 break
             }
-            
+
         }
-        
+
     }
-    
+
     // MARK: - Resources
-    
-    /// Updates the displayed resources.
+
+    /// Name of the resource to display.
+    private var resourceName: String? {
+        didSet {
+            if resourceName != nil {
+                updateResources(completion: nil)
+            }
+        }
+    }
+
+    /// Updates image view with the specified resource.
     ///
     /// - note:
     /// Asynchronous network operation.
     ///
-    /// - Parameter completion: <#completion description#>
+    /// - Parameter completion: The completion handler is called once the image is downloaded (or an error is
+    ///                         encountered).
     private func updateResources(completion: ((Error?) -> Void)?) {
-        
+
         // extract resource model
-        guard let resourceModel = vatom.props.resources.first(where: { $0.name == config.imageName }) else {
+        guard let resourceModel = vatom.props.resources.first(where: { $0.name == resourceName }) else {
             return
         }
-        
+
         // encode url
         guard let encodeURL = try? BLOCKv.encodeURL(resourceModel.url) else {
             return
         }
-        
-        //FIXME: Where should this go?
-        ImagePipeline.Configuration.isAnimatedImageDataEnabled = true
-        
-        //TODO: Should the size of the VatomView be factoring in and the image be resized?
-        
+
         // load image (automatically handles reuse)
         Nuke.loadImage(with: encodeURL, into: self.animatedImageView) { (_, error) in
             self.isLoaded = true
             completion?(error)
         }
-        
+
     }
-    
+
 }
 
 // MARK: - Image Policy
@@ -208,7 +218,7 @@ private protocol ImagePolicy {
 }
 
 private extension ImagePolicyFaceView {
-    
+
     /*
      Example Payload
      
@@ -234,22 +244,22 @@ private extension ImagePolicyFaceView {
      }
      
      */
-    
+
     struct Config {
-        
+
         // MARK: - Structs
-        
+
         /*
          Strongly typed struct for each known image policy type.
          */
-        
+
         struct ChildCount: ImagePolicy {
             /// Name of the resource the policy requires for display.
             let resourceName: String
             /// Maximum number of children for which the policy is valid.
             let countMax: Int
         }
-        
+
         struct FieldLookup: ImagePolicy {
             /// Name of the resource the policy requires for display.
             let resourceName: String
@@ -258,37 +268,37 @@ private extension ImagePolicyFaceView {
             /// Property value to campare to.
             let value: Any
         }
-        
+
         struct Fallback: ImagePolicy {
             /// Name of the resource the policy requires for display.
             let resourceName: String
         }
-        
+
         /// Array of image policies
         var policies: [ImagePolicy] = []
-        
+
         // MARK: - Initializer
-        
+
         /// Initialize using default values.
         init() {}
-        
+
         /// Initialize using face configuration.
         init(_ faceConfig: JSON) {
-            
+
             // ensure an image policy array is present
             guard let imagePolicyDescriptors = faceConfig["image_policy"]?.arrayValue else {
                 return
             }
-            
+
             // loop over all the polices
             for imagePolicyDescriptor in imagePolicyDescriptors {
-                
+
                 // ensure a resource name is present
                 guard let resourceName = imagePolicyDescriptor["resource"]?.stringValue else {
                     // skip this policy
                     continue
                 }
-                
+
                 // child count
                 if let countMax = imagePolicyDescriptor["count_max"]?.floatValue {
                     let childCountPolicy = ChildCount(resourceName: resourceName, countMax: Int(countMax))
@@ -307,15 +317,45 @@ private extension ImagePolicyFaceView {
                     let fallbackPolicy = Fallback(resourceName: resourceName)
                     self.policies.append(fallbackPolicy)
                 }
-                
+
             }
-            
+
             // This is the last resort fallback (evaluated *after* the face developer assigned fallback).
             let activatedImageFallbackPolicy = Fallback(resourceName: "ActivatedImage")
             self.policies.append(activatedImageFallbackPolicy)
-            
+
         }
-        
+
     }
-    
+
+}
+
+extension ImagePolicyFaceView: VatomObserverDelegate {
+
+    func vatomObserver(_ observer: VatomObserver, rootVatomStateUpdated: VatomModel) {
+        print(#function)
+
+    }
+
+    func vatomObserver(_ observer: VatomObserver, childVatomStateUpdated: VatomModel) {
+        print(#function)
+
+    }
+
+    func vatomObserver(_ observer: VatomObserver, willAddChildVatom vatomID: String) {
+        print(#function)
+
+    }
+
+    func vatomObserver(_ observer: VatomObserver, didAddChildVatom childVatom: VatomModel) {
+        print(#function)
+        self.debouncedUpdateUI()
+    }
+
+    func vatomObserver(_ observer: VatomObserver, didRemoveChildVatom childVatom: VatomModel) {
+        print(#function)
+        // debounce since the "split" action causes state updates for all children to come through in close proximity
+        self.debouncedUpdateUI()
+    }
+
 }

5edc91ae3b7c6c039686590187bd80615aae9022
Cameron McOnie <cjmconie@gmail.com>
2018-10-26 07:24:43 +0200

    raise access level

--- BlockV/Core/Helpers/KeyPath.swift
+++ BlockV/Core/Helpers/KeyPath.swift
@@ -12,14 +12,14 @@
 import Foundation
 
 /// Simple struct that models a key path.
-struct KeyPath: Equatable {
+public struct KeyPath: Equatable {
     private(set) var segments: [String]
-    
+
     var isEmpty: Bool { return segments.isEmpty }
     var path: String {
         return segments.joined(separator: ".")
     }
-    
+
     /// Strips off the first segment and returns a pair consisting of the first segment and the remaining key path.
     /// Returns `nil` if the key path has no segments.
     func headAndTail() -> (head: String, tail: KeyPath)? {
@@ -28,17 +28,17 @@ struct KeyPath: Equatable {
         let head = tail.removeFirst()
         return (head, KeyPath(segments: tail))
     }
-    
+
 }
 
 /// Initializes a KeyPath with a string of the form "this.is.a.keypath"
-extension KeyPath {
-    
+public extension KeyPath {
+
     /// This init is only required for testing - file access is limited to internal to allow for testing export
     init() {
         segments = []
     }
-    
+
     init(_ string: String) {
         segments = string.components(separatedBy: ".")
     }
@@ -46,13 +46,13 @@ extension KeyPath {
 
 /// Initializ a KeyPath using a string literal.
 extension KeyPath: ExpressibleByStringLiteral {
-    init(stringLiteral value: String) {
+    public init(stringLiteral value: String) {
         self.init(value)
     }
-    init(unicodeScalarLiteral value: String) {
+    public init(unicodeScalarLiteral value: String) {
         self.init(value)
     }
-    init(extendedGraphemeClusterLiteral value: String) {
+    public init(extendedGraphemeClusterLiteral value: String) {
         self.init(value)
     }
 }

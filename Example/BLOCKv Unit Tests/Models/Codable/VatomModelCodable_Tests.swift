91b4ebb785611b759a597eaea0805fc0a1d8473f
Cameron McOnie <cjmconie@gmail.com>
2018-11-24 10:14:01 +0200

    organise tests in logical groups

--- Example/BLOCKv Unit Tests/Models/Codable/VatomModelCodable_Tests.swift
+++ Example/BLOCKv Unit Tests/Models/Codable/VatomModelCodable_Tests.swift
@@ -82,11 +82,4 @@ class VatomModelCodable_Tests: XCTestCase {
         
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

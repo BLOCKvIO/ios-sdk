d3eca78789a3b925d903ee4d880cc2d284a82561
Cameron McOnie <cjmconie@gmail.com>
2019-04-06 12:17:31 +0200

    Fix policy count max check Docs

--- BlockV/Core/Data Pool/Helpers/Vatom+Containment.swift
+++ BlockV/Core/Data Pool/Helpers/Vatom+Containment.swift
@@ -193,43 +193,52 @@ extension VatomModel {
 // MARK: - Containment
 
 extension VatomModel {
-    
-    /// Returns `true` if the vatom will accept a request to contain the child vatom, `false` otherwise.
+
+    /// Returns `true` if the container vatom will accept a request to contain the child vatom, `false` otherwise.
+    ///
+    /// Only applicable to *owned* vatoms.
     public func canContainChild(_ childVatom: VatomModel) -> Bool {
-        
+
+        // ensure this vatom is a container
+        if !self.isContainer { return false }
+
         // folder containers will always accept requests to contain children
         if self.rootType == .container(.folder) { return true }
-        
+
         // defined containers will only accept requests to contain vatoms matching their child policy rules
         if self.rootType == .container(.defined) {
             return doesChildPolicyAllowContainmentOf(childVatom)
         }
-        
+
         // standard vatoms cannot contain children
         // discover and package containers will not accept requests to contain children
         return false
-        
+
     }
-    
-    /// Returns `true` if this vatom's child policy permits containment of the child vatom. `false` otherwise.
+
+    /// Returns `true` if this container vatom's child policy permits containment of the child vatom. `false` otherwise.
+    ///
+    /// Only applicable to *owned* vatoms.
     private func doesChildPolicyAllowContainmentOf(_ childVatom: VatomModel) -> Bool {
-        
+
         // check if any policies match this template variation
         for policy in self.props.childPolicy where policy.templateVariationID == childVatom.props.templateVariationID {
-            
+
             // check if there is a maximum number of children
             if policy.creationPolicy.enforcePolicyCountMax {
-                //FIXME: Check against child count
-                return false
+                // check if current child count is less then policy max
+                if policy.creationPolicy.policyCountMax > self.listCachedChildren().count {
+                    return true
+                }
             } else {
                 return true
             }
         }
-        
+
         return false
-        
+
     }
-    
+
     /// Returns `true` if the vatom's root type is a 'Container' type.
     ///
     /// Container vatoms have the ability to have parent-child relationships.
@@ -239,10 +248,10 @@ extension VatomModel {
         }
         return false
     }
-    
+
     /// Enum modeling the know root type.
     private var rootType: RootType {
-        
+
         if self.props.rootType == "vAtom::vAtomType" {
             return .standard
         } else {
@@ -258,20 +267,20 @@ extension VatomModel {
                 return .unknown
             }
         }
-        
+
     }
-    
+
     private enum RootType: Equatable {
         case standard
         case container(ContainerType)
         case unknown
     }
-    
+
     private enum ContainerType: Equatable {
         case folder
         case package
         case discover
         case defined
     }
-    
+
 }

9cb3088a44d5808f7da3594bfd261473c7b9fa0e
Cameron McOnie <cjmconie@gmail.com>
2019-06-05 11:24:12 +0200

    Lint

--- BlockV/Core/Debug/DebugHUDViewController.swift
+++ BlockV/Core/Debug/DebugHUDViewController.swift
@@ -17,19 +17,19 @@ import BLOCKv
 /// All events are passed through to underlying windows expect if the view conforms the `FloatingView` protocol. In this
 /// case, the touch events will forwarded to the view itself.
 class FloatingHUDWindow: UIWindow {
-    
+
     init() {
         super.init(frame: UIScreen.main.bounds)
         backgroundColor = nil
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     /// Determine whether the hit test should be
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        
+
         let hitView = super.hitTest(point, with: event)
         // check if this window should handle this event
         if hitView!.self is FloatingView {
@@ -37,9 +37,9 @@ class FloatingHUDWindow: UIWindow {
         }
         // indicates this window does not handle this event
         return nil
-        
+
     }
-    
+
 }
 
 /// A view controller which provides a context for a floating HUD.
@@ -49,25 +49,24 @@ class FloatingHUDWindow: UIWindow {
 /// HUD content should be added as a subview to the view controller's view. If the view's need to capture touch events
 /// they should conform to the `FloatingView` protocol.
 class DebugHUDViewController: UIViewController {
-    
+
     // MARK: - Properties
-    
+
     lazy var socketContentView: SocketContentView = {
         let view = SocketContentView()
         view.cornerRadius = 8
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor.gray.withAlphaComponent(0.4)
-        
+
         return view
     }()
-    
+
     private let window = FloatingHUDWindow()
-    
+
     // MARK: - Initializer
-    
+
     init() {
         super.init(nibName: nil, bundle: nil)
-        //        window.frame = CGRect(x: 0, y: 0, width: 500, height: 500)
         window.windowLevel = UIWindow.Level(rawValue: CGFloat.greatestFiniteMagnitude)
         window.isHidden = false
         window.rootViewController = self
@@ -75,69 +74,69 @@ class DebugHUDViewController: UIViewController {
                                                name: UIResponder.keyboardDidShowNotification,
                                                object: nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - Life cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.setup()
     }
-    
+
     var socketContentViewCenterXContraint: NSLayoutConstraint?
     var socketContentViewCenterYContraint: NSLayoutConstraint?
-    
+
     private func setup() {
-        
+
         // socket content
         self.view.addSubview(socketContentView)
-        
+
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panDidFire))
         socketContentView.addGestureRecognizer(panGesture)
-        
+
         // constraints
         socketContentViewCenterXContraint = socketContentView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor,
                                                                                        constant: 0)
         socketContentViewCenterXContraint?.isActive = true
-        
+
         socketContentViewCenterYContraint = socketContentView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor,
                                                                                        constant: 0)
         socketContentViewCenterYContraint?.isActive = true
-        
+
         socketContentView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.25).isActive = true
         socketContentView.widthAnchor.constraint(equalTo: socketContentView.heightAnchor, multiplier: 1).isActive = true
 
     }
-    
+
     // MARK: - Section
-    
+
     @objc func panDidFire(pan: UIPanGestureRecognizer) {
-        
+
         socketContentView.layoutIfNeeded()
-        
+
         let offset = pan.translation(in: self.view)
         pan.setTranslation(CGPoint.zero, in: self.view)
-        
+
         socketContentViewCenterXContraint?.constant += offset.x
         socketContentViewCenterYContraint?.constant += offset.y
-        
+
     }
-    
+
     @objc func keyboardDidShow(note: NSNotification) {
         window.windowLevel = UIWindow.Level(rawValue: 0)
         window.windowLevel = UIWindow.Level(rawValue: CGFloat.greatestFiniteMagnitude)
     }
-    
+
 }
 
 // MARK: - Helpers
 
 /// Protocol that indicates a view is a floating view. This allows for hit testing.
-protocol FloatingView where Self: UIView  { }
+protocol FloatingView where Self: UIView { }
 
 /// Simple subclass of UIView that conforms to FloatingView. Use this subsclass to add interactable content on a
 /// floating window.
--- BlockV/Core/Debug/SocketInstrument.swift
+++ BlockV/Core/Debug/SocketInstrument.swift
@@ -19,124 +19,123 @@ enum HUDStatus {
 
 /// Controller object that interacts with the web socket.
 class SocketInstrument {
-    
+
     private let pingInterval: TimeInterval = 5
-    
+
     private var timer: Timer!
-    
+
     fileprivate var statusHandler: ((_ status: HUDStatus) -> Void)?
-    
+
     init() {
-        
+
         // listen to web socket lifecycle events
         BLOCKv.socket.onDisconnected.subscribe(with: self) { [weak self] _ in
             self?.statusHandler?(.red)
         }
-        
+
         BLOCKv.socket.onConnected.subscribe(with: self) { [weak self] _ in
             self?.statusHandler?(.green)
         }
-        
+
         DispatchQueue.main.async {
             self.activate(pingInterval: self.pingInterval)
         }
     }
-    
+
     private func activate(pingInterval: TimeInterval) {
-        
+
         // create timer
-        self.timer = Timer.scheduledTimer(withTimeInterval: pingInterval, repeats: true) { [weak self] timer in
-            
+        self.timer = Timer.scheduledTimer(withTimeInterval: pingInterval, repeats: true) { [weak self] _ in
+
             /*
              Technical:
              A pong must
              */
-            
+
             var didTimeout = false
-            
+
             BLOCKv.socket.writePing(completion: {
-                
+
                 if didTimeout {
                     self?.statusHandler?(.red)
                 } else {
                     self?.statusHandler?(.green)
                 }
-                
+
             })
-            
+
             // fire a timeout 0.1 seconds before the next ping
             DispatchQueue.main.asyncAfter(deadline: .now() + pingInterval - 0.1, execute: {
                 didTimeout = true
             })
-            
+
         }
-        
+
     }
-    
-    
+
 }
 
 // MARK: - Socket Content View
 
 class SocketContentView: RoundedView {
-    
+
     // MARK: - Properties
-    
+
     private var didSetupConstraints = false
-    
+
     lazy var statusDotView: CircleView = {
         let view = CircleView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .red
         return view
     }()
-    
+
     // controller object to interact with the model
     var socketInstrument: SocketInstrument!
-    
+
     // MARK: - Initializer
-    
+
     init() {
         super.init(frame: CGRect.zero)
         self.commonInit()
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.commonInit()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func commonInit() {
         self.addSubview(statusDotView)
         self.setNeedsUpdateConstraints()
         self.socketInstrument = SocketInstrument()
-        
+
         socketInstrument.statusHandler = pulse(status:)
     }
-    
+
     // MARK: - Lifecycle
-    
+
     override func updateConstraints() {
-        
+
         if !didSetupConstraints {
-            
+
             statusDotView.widthAnchor.constraint(equalToConstant: 12).isActive = true
             statusDotView.heightAnchor.constraint(equalToConstant: 12).isActive = true
             statusDotView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12).isActive = true
             statusDotView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12).isActive = true
-            
+
             didSetupConstraints = true
         }
-        
+
         super.updateConstraints()
     }
-    
+
     // MARK: - Updates
-    
+
     private func pulse(status: HUDStatus) {
         switch status {
         case .green:
@@ -148,31 +147,31 @@ class SocketContentView: RoundedView {
         }
         statusDotView.pulse()
     }
-    
+
 }
 
 // MARK: - Helper Views
 
 /// Create a view with rounded edges.
 class RoundedView: ContentView {
-    
+
     var cornerRadius: CGFloat = 0
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = cornerRadius
     }
-    
+
 }
 
 /// Creates a round UIView.
 class CircleView: UIView {
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = self.bounds.size.width/2
     }
-    
+
 }
 
 extension CircleView: Pulseable { }
@@ -182,7 +181,7 @@ protocol Pulseable where Self: UIView {
 }
 
 extension Pulseable {
-    
+
     func pulse() {
         let pulseAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
         pulseAnimation.duration = 1
@@ -193,5 +192,5 @@ extension Pulseable {
         pulseAnimation.repeatCount = 1
         self.layer.add(pulseAnimation, forKey: "animateOpacity")
     }
-    
+
 }

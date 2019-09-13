8c0acf9d65478587f55da70a6d86a21d17f555f5
Cameron McOnie <cjmconie@gmail.com>
2019-09-13 14:38:36 +0200

    Add regular expression split extension

---
+++ BlockV/Core/Extensions/String+Core Etx.swift
@@ -0,0 +1,33 @@
+//
+//  BlockV AG. Copyright (c) 2018, all rights reserved.
+//
+//  Licensed under the BlockV SDK License (the "License"); you may not use this file or
+//  the BlockV SDK except in compliance with the License accompanying it. Unless
+//  required by applicable law or agreed to in writing, the BlockV SDK distributed under
+//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+//  ANY KIND, either express or implied. See the License for the specific language
+//  governing permissions and limitations under the License.
+//
+
+import Foundation
+
+extension String {
+
+    /// Returns an array of string split using the specified regular expresion.
+    ///
+    /// Throws if the regular expression pattern is invalid.
+    func split(usingRegex pattern: String) throws -> [String] {
+        let regex = try NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: self, range: NSRange(0..<utf16.count))
+        let ranges = [startIndex..<startIndex] + matches.map{Range($0.range, in: self)!} + [endIndex..<endIndex]
+        return (0...matches.count).map {String(self[ranges[$0].upperBound..<ranges[$0+1].lowerBound])}
+    }
+
+    /// Returns an array of strings split using the regular expression.
+    func split(usingRegex regex: NSRegularExpression) -> [String] {
+        let matches = regex.matches(in: self, range: NSRange(0..<utf16.count))
+        let ranges = [startIndex..<startIndex] + matches.map{Range($0.range, in: self)!} + [endIndex..<endIndex]
+        return (0...matches.count).map {String(self[ranges[$0].upperBound..<ranges[$0+1].lowerBound])}
+    }
+
+}
